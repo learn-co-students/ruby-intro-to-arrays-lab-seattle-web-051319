8a411e884054d9a96fc76c6be30dfc428f0c057b
Bryna Dunnells <badunnells@gmail.com>
2019-05-02 22:22:19 +0000

    those array methods fo js are helpful here too

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,30 @@
+
+ def instantiate_new_array
+   new = []
+
+end
+
+def array_with_two_elements
+  new = ["boxes", "tape"]
+end
+
+def first_element(array)
+  return array.first
+end
+
+def third_element(array)
+  return array[2]
+end
+def last_element(array)
+  array[-1]
+end
+def first_element_with_array_methods(array)
+      array.first
+end
+def last_element_with_array_methods(array)
+  array.last
+
+end
+def length_of_array(array)
+  array.length
+end
