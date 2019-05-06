a668fca85609aa5ecaf7b6839d6f5d89f041312a
Danielle Martinoli <daniechris5@gmail.com>
2019-05-06 18:54:06 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,7 +3,7 @@ def instantiate_new_array
 end
   return instantiate_new_array
 
-def array_with_two_elements(element)
+def array_with_two_elements(array, element)
   []
 end
   return array_with_two_elements
