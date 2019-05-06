b376047c77d6e8ce522d20a4fa1f1b66d0a9e2f2
Danielle Martinoli <daniechris5@gmail.com>
2019-05-06 19:32:42 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,9 +1,31 @@
 def instantiate_new_array
-  []
+  my_new_array = []
 end
-  return instantiate_new_array
 
-def array_with_two_elements(array, element)
-  array = [element1, element2]
+def array_with_two_elements
+  array = [0,1]
 end
-  return array_with_two_elements
+
+def my_first_element
+  array.[0]
+end
+
+def third_element
+  array[2]
+end
+
+def last_element
+  array.[-1]
+end
+
+def first_element_with_array_methods
+  array.first
+end
+
+def last_element_with_array_methods
+  array.last
+end
+
+def length_of_array
+  array.length
+end
